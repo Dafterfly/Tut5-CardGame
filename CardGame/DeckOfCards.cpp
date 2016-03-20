309fcd6ab4bb81b2e35f59dc97b6c2540830e4ea
Eustacia Rajman <eustacia_rajman@telkomsa.net>
2016-03-20 12:46:23 +0200

    Add design project

--- CardGame/DeckOfCards.cpp
+++ CardGame/DeckOfCards.cpp
@@ -8,24 +8,24 @@ using namespace std;
 DeckOfCards::DeckOfCards() // initialise a deck of cards
 {
 	noOfCards = 20; //Start with a full deck again 
-	cardPtr = new Card[noOfCards];
+	ptrCard = new Card[noOfCards];
 
 	// first half of deck is red 1-10
 		 	for (int i = 0; i < 10; i++)
 		 	{
-				cardPtr[i] = Card(i + 1, "Red");
+				ptrCard[i] = Card(i + 1, "Red");
 		 	}
 
 	 	//second half of deck is black 1-10
 	 	for (int i = 10; i < 20; i++)
 	 	{
-			cardPtr[i] = Card((i + 1) - 10, "Black");
+			ptrCard[i] = Card((i + 1) - 10, "Black");
 	 	}
-	
-
 }
 
 
 DeckOfCards::~DeckOfCards()
 {
+	delete[] ptrCard;
 }
+
