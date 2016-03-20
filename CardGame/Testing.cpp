4196ec35ac6be89ec7a8da7cba51e505fb9f6188
Eustacia Rajman <eustacia_rajman@telkomsa.net>
2016-03-20 14:12:13 +0200

    Test peek, shuffle and draw

--- CardGame/Testing.cpp
+++ CardGame/Testing.cpp
@@ -12,7 +12,7 @@ using namespace std;
 int main() // main testing programme
 {
 	// Testing card class:
-	
+	cout <<"Testing card class:" << endl;
 	// Normal
 	cout << "Normal conditions: ";
 	Card card1(10, "Red");
@@ -37,7 +37,31 @@ int main() // main testing programme
 	Card card4(111, "Purple");
 	cout << endl;
 
+	// DeckOrCards class testing:
+	cout << "Testing DeckOrCards class:" << endl;
+	DeckOfCards *myDeck = new DeckOfCards();
+	myDeck->print();
+	cout << "Number of cards Remaining: " << myDeck->numberOfCards() << endl;
 
+	// test peek:
+	cout << "Peek: ";
+	myDeck->peek();
+	cout << endl;
+	
+	// test shuffle
+	cout << "Shuffle: "<<endl;
+	myDeck->shuffle();
+	myDeck->print();
+	cout << endl;
+	
+	// test draw
+	cout << "Draw: ";
+	myDeck->draw();
+	cout << endl;
+	myDeck->print();
+	cout << endl;
+	cout << "Number of cards Remaining: " << myDeck->numberOfCards() << endl;
+	cout << endl;
 
 	return 0;
 }
