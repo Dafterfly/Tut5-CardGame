3d89e5aaf15828f1a5ad5d3ac5fd78a4d7be09ec
Eustacia Rajman <eustacia_rajman@telkomsa.net>
2016-03-20 13:32:25 +0200

    Add error-checking for card constructor

--- CardGame/Card.cpp
+++ CardGame/Card.cpp
@@ -4,11 +4,32 @@
 
 using namespace std;
 
+Card::Card() 
+{
+	
+}
 
 Card::Card(int numberIn, string colourIn) // overload default constructor
 {
-	num = numberIn;
-	col = colourIn;
+	// check if numberIn falls within range
+	if (numberIn >= 1 && numberIn <= 10)
+	{
+		num = numberIn;
+	}
+	else
+	{ 
+		cout <<"The number entered does not fall within range"<< endl;
+	}
+
+	// check if colourIn is valid
+	if (colourIn.compare("Black") == 0 || colourIn.compare("Red")==0)
+	{
+		col = colourIn;
+	}
+	else
+	{
+		cout << "The colour entered is not valid" << endl;
+	}
 }
 
 
