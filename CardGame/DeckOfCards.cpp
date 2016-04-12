1c3814f009522bfb9217531ce14fdedd478672d7
Eustacia Rajman <214514354@stu.ukzn.ac.za>
2016-04-12 14:17:59 +0200

    Attempt Initialising deck, peek, draw and shuffle using vectors

--- CardGame/DeckOfCards.cpp
+++ CardGame/DeckOfCards.cpp
@@ -2,24 +2,27 @@
 #include "Card.h"
 #include <iostream>
 #include <ctime>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 DeckOfCards::DeckOfCards() // initialise a deck of cards
 {
 	noOfCards = 20; //Start with a full deck again 
-	ptrCard = new Card[noOfCards];
-	
+	//ptrCard = new Card[noOfCards];
+	Card tempCard;
 	// first half of deck is red 1-10
 	for (int i = 0; i < 10; i++)
 	{
-		ptrCard[i] = Card(i + 1, "Red");
+		 tempCard(i + 1, "Red")
+			 myDeck.push_back(tempCard);
 	}
 	
 	//second half of deck is black 1-10
 	for (int i = 10; i < 20; i++)
 	{
-		ptrCard[i] = Card((i + 1) - 10, "Black");
+		myDeck.push_back(Card(i + 1, "Red"));
 	}
 }
 
@@ -35,39 +38,22 @@ DeckOfCards DeckOfCards::reset()// makes a new deck
 	return DeckOfCards(); 
 }
 
-Card DeckOfCards::draw() // draws a card
+void DeckOfCards::draw() // draws a card
 {
-	ptrCard[noOfCards - 1].print(); // displays card
-	noOfCards--;  //Decrement card count 
-	return ptrCard[noOfCards];
-	delete &ptrCard[noOfCards]; // removes card from deck
+	myDeck.back().print();
+	(myDeck.pop_back());
 }
 
-Card DeckOfCards::peek() // prints card at top of deck (like draw but des not delete the card)
+void DeckOfCards::peek() // prints card at top of deck (like draw but des not delete the card)
 {
-	ptrCard[noOfCards - 1].print();
-	return ptrCard[noOfCards - 1];
+	(myDeck.back()).print();
 }
 
 void DeckOfCards::shuffle() //picks two random cards and switches their order repeats this 50 times 
 {
 	if (noOfCards >= 2)
 	{
-		srand(time(NULL));  //seed the randomizer  
-		Card temp;
-		int r1 = 0; //first random card index
-		int r2 = 0; //second random card index
-		for (int i = 0; i < 50; i++)
-		{
-			r1 = rand() % noOfCards;
-			r2 = rand() % noOfCards;
-			if (r1 != r2) // swap cards if index is not the same
-			{
-				temp = ptrCard[r1];
-				ptrCard[r1] = ptrCard[r2];
-				ptrCard[r2] = temp;
-			}
-		}
+		random_shuffle(myDeck.begin(), myDeck.end());
 	}
 	else
 	cout <<"There are not enough cards to shuffle!"<< endl;
--- CardGame/DeckOfCards.h
+++ CardGame/DeckOfCards.h
@@ -1,18 +1,20 @@
 #pragma once
 
 #include "Card.h" // in order to make Card objects
+#include <vector>
 
 class DeckOfCards
 {
 private: int noOfCards;
 		 Card *ptrCard = NULL;  //card stack / array pointer
+		 vector<Card> myDeck;
 public:
 	DeckOfCards();
 	~DeckOfCards();
 	DeckOfCards reset();  
 	void shuffle();
-	Card draw();   
-	Card peek();  
+	void draw();   
+	void peek();  
 	int numberOfCards();
 	void print();
 };
